b0e20c5877e093bc494790f9d747fa72b05b51fd
Benjamin Smith <bsmith@cabbage.org.uk>
2023-01-16 16:22:26 +0000

    MVP for Water Bottle

--- src/main/java/WaterBottle.java
+++ src/main/java/WaterBottle.java
@@ -1,2 +1,23 @@
 public class WaterBottle {
+    private int volume;
+
+    public WaterBottle() {
+        volume = 100;
+    }
+
+    public void drink() {
+        volume -= 10;
+    }
+
+    public void fill() {
+        volume = 100;
+    }
+
+    public void empty() {
+        volume = 0;
+    }
+
+    public int volumeFilledPercentage() {
+        return volume;
+    }
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,44 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class WaterBottleTest {
+    private WaterBottle theBottle;
+
+    @Before
+    public void setUp() {
+        theBottle = new WaterBottle();
+    }
+
+    /* The JUnit FAQ suggests not testing very simple accessors that are "too simple to break":
+        https://junit.org/junit4/faq.html#best_3
+        `volumeFilledPercentage` is acting as getter, but named with a bit more abstraction.
+        However, it gets called everywhere so gets tested as part of that.
+     */
+
+    @Test
+    public void startsFilled() {
+        assertEquals(100, theBottle.volumeFilledPercentage());
+    }
+
+    @Test
+    public void drinkingReducesVolumeFilledBy10Percent() {
+        theBottle.drink();
+        assertEquals(90, theBottle.volumeFilledPercentage());
+    }
+
+    @Test
+    public void canBeEmptied() {
+        theBottle.empty();
+        assertEquals(0, theBottle.volumeFilledPercentage());
+    }
+
+    @Test
+    public void canBeFilledAfterEmptying() {
+        theBottle.empty();
+        assertEquals(0, theBottle.volumeFilledPercentage());
+        theBottle.fill();
+        assertEquals(100, theBottle.volumeFilledPercentage());
+    }
 }
