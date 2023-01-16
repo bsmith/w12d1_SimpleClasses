a238568cfbb611471a56fc7db38a600a28bac70b
Benjamin Smith <bsmith@cabbage.org.uk>
2023-01-16 16:32:41 +0000

    Printing pages

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,26 @@
 public class Printer {
+    private int sheetsOfPaperLeft;
+    private int tonerVolume;
+
+    public Printer(int sheetsOfPaperLeft, int tonerVolume) {
+        this.sheetsOfPaperLeft = sheetsOfPaperLeft;
+        this.tonerVolume = tonerVolume;
+    }
+
+    public boolean print(int pages, int copies) {
+        int sheetsToUse = pages * copies;
+        if (sheetsToUse > sheetsOfPaperLeft)
+            return false;
+        sheetsOfPaperLeft -= sheetsToUse;
+        return true;
+    }
+
+    /*** GETTERS ***/
+    public int getSheetsOfPaperLeft() {
+        return sheetsOfPaperLeft;
+    }
+
+    public int getTonerVolume() {
+        return tonerVolume;
+    }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class PrinterTest {
+    private Printer thePrinter;
+
+    @Before
+    public void setUp() {
+        thePrinter = new Printer(250, 1000);
+    }
+
+    @Test
+    public void canPrintShortDocument() {
+        boolean retval = thePrinter.print(10, 2);
+        assertTrue(retval);
+        assertEquals(230, thePrinter.getSheetsOfPaperLeft());
+    }
+
+    @Test
+    public void cannotPrintLongDocument() {
+        boolean retval = thePrinter.print(100, 3);
+        assertFalse(retval);
+        assertEquals(250, thePrinter.getSheetsOfPaperLeft());
+    }
 }
